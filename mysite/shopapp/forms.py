06ccaa467e96ee921b3b855e4e19a6f649773c73
asimzhan <atossoos@gmail.com>
2025-11-22 21:37:40 +0500

    Update ProductForm: change images field widget to ClearableFileInput for improved user experience

--- mysite/shopapp/forms.py
+++ mysite/shopapp/forms.py
@@ -9,7 +9,8 @@ class ProductForm(forms.ModelForm):
         fields = "name", "price", "description", "discount", "preview"
 
     images = forms.ImageField(
-        widget=forms.FileInput(attrs={"multiple": True}),
+        widget=forms.ClearableFileInput(),
+        required=False,  
     )
 
 
