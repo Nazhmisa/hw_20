fcf365ef1ec050bcab1d486ae771af507c0013d1
asimzhan <atossoos@gmail.com>
2025-11-22 17:44:09 +0500

    Update ProductForm: change images field to FileField for multiple file uploads

--- shopapp/forms.py
+++ shopapp/forms.py
@@ -7,9 +7,10 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = "name", "price", "description", "discount", "preview"
-
-    images = forms.ImageField(
-        widget=forms.ClearableFileInput(attrs={"multiple": True}),
+        
+    images = forms.FileField(
+        widget=forms.FileInput(attrs={"multiple": True}),
+        required=False,
     )
 
 
