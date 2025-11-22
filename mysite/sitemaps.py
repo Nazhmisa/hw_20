17539a9efb15b0977508fd7255c711a6ec351649
asimzhan <atossoos@gmail.com>
2025-11-22 17:52:31 +0500

    Add sitemaps for shop and site: implement ShopSitemap and SiteSitemap classes

--- mysite/sitemaps.py
+++ mysite/sitemaps.py
@@ -1,5 +1,28 @@
-from blogapp.sitemap import BlogSitemap
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+from shopapp.models import Product
+
+
+class ShopSitemap(Sitemap):
+    changefreq = "never"
+    priority = 0.5
+
+    def items(self):
+        return Product.objects.filter(archived=False)
+
+
+class SiteSitemap(Sitemap):
+    changefreq = "never"
+    priority = 0.5
+
+    def items(self):
+        return ["about-me"]
+
+    def location(self, obj):
+        return reverse(obj)
+
 
 sitemaps = {
-    "blog": BlogSitemap,
+    "shop": ShopSitemap,
+    "site": SiteSitemap,
 }
