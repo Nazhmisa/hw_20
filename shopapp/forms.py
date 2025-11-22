81219e5bcfede33fef3193052c3ac8adf0e727dd
asimzhan <atossoos@gmail.com>
2025-11-22 17:46:16 +0500

    Refactor ProductForm: update images field to allow multiple file uploads

--- shopapp/forms.py
+++ shopapp/forms.py
@@ -1,6 +1,5 @@
 from django import forms
-
-from shopapp.models import Product
+from .models import Product, Order
 
 
 class ProductForm(forms.ModelForm):
@@ -9,10 +8,16 @@ class ProductForm(forms.ModelForm):
         fields = "name", "price", "description", "discount", "preview"
         
     images = forms.FileField(
-        widget=forms.FileInput(attrs={"multiple": True}),
+        widget=forms.FileInput(),
         required=False,
     )
 
 
 class CSVImportForm(forms.Form):
     csv_file = forms.FileField()
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = "delivery_address", "promocode", "user", "products"
