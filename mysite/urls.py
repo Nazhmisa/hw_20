23daf548c9ce1290456ea8bcddf29e554e33cead
asimzhan <atossoos@gmail.com>
2025-11-22 18:23:10 +0500

    Enhance ShopApp UI and functionality
    - Redirect root URL to shop page
    - Update base template with Bootstrap and Font Awesome for improved styling
    - Revamp groups list page with better layout and user experience
    - Redesign order detail page for clearer presentation of order information
    - Improve order list page with card layout and better visibility of order details
    - Enhance product confirm delete page with a more user-friendly design
    - Revamp product form and update form for consistency and better user experience
    - Update product details page with improved layout and additional product information
    - Revise products list page to include a card layout and better product visibility
    - Modify products changelist template for better integration with admin features
    - Enhance shop index page with a hero section and featured products layout

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,10 +18,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
-
+from django.views.generic import RedirectView
 from .sitemaps import sitemaps
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/shop/', permanent=True)),
     path('admin/', admin.site.urls),
     path('shop/', include('shopapp.urls')),
     path('myauth/', include('myauth.urls')),
